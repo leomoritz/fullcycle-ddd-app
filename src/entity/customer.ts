7e9d58e1ffb274a2201a6fa7a93cdb2bcfcd5763
Leônidas Moritz Rosa <leogui.moritz@gmail.com>
2025-06-08 19:54:18 -0300

    Principio da auto-validação para garantir a consistência da entidade

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -12,19 +12,41 @@ class Customer {
   _id: string;
   _name: string;
   _address: string;
-  _active: boolean = true;
+  _active: boolean = false;
 
   constructor(id: string, name: string, address: string) {
     this._id = id;
     this._name = name;
     this._address = address;
+    this.validate();
   }
-  
+
+  validate(): void {
+    if (!this._id) {
+      throw new Error('ID is required');
+    }
+    if (!this._name) {
+      throw new Error('Name is required');
+    }
+    if (!this._address) {
+      throw new Error('Address is required');
+    }
+  }
+
   changeName(name: string): void {
     this._name = name;
+    this.validate();
+  }
+
+  changeAddress(address: string): void {
+    this._address = address;
+    this.validate();
   }
 
   activate(): void {
+    if (this._address.length === 0) {
+        throw new Error('Cannot activate customer without address');
+    }
     this._active = true;
   }
 
